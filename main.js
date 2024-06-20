90b8f0b48b048aef43db7ec8935b8994e2d6a278
SilverCanvas72 <idewij27@igs.vic.edu.au>
2024-06-20 21:02:54 +1000

    Finished! Added all statues bars and fixed styling
    Finished all progress bars.
    Added a status at the bottom of each bar. (eg. 35 / 75 )
    Fixed Styling Issues

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ var x = setInterval(function() {
 const decreaseBtn = document.getElementById("decreaseBtn");
 const increaseBtn = document.getElementById("increaseBtn");
 const ticketsLabel = document.getElementById("ticketsAmount");
+
 let tickets = 0;
 
 increaseBtn.onclick = function(){
@@ -38,6 +39,11 @@ increaseBtn.onclick = function(){
     ticketsLabel.textContent = `${tickets} Tickets`;
     updateStats3()
     updateStatsOP()
+    updateStatsW()
+    updateStatsB()
+    updateStatsK()
+    updateStatsFS()
+    updateStats13()
 }
 
 decreaseBtn.onclick = function(){
@@ -45,20 +51,71 @@ decreaseBtn.onclick = function(){
     ticketsLabel.textContent = `${tickets} Tickets`;
     updateStats3()
     updateStatsOP()
+    updateStatsW()
+    updateStatsB()
+    updateStatsK()
+    updateStatsFS()
+    updateStats13()
 }
 
 const updateStats3 = () => {
     const progress3 = (tickets/200)* 100
     const progressBar3 = document.getElementById('progressQuest3')
-
     progressBar3.style.width = `${progress3}%`
+    const metaLabel = document.getElementById("quest3Progress");
+    metaLabel.textContent = `${tickets} / 200`;
+
 }
 
 const updateStatsOP = () => {
-
     const progressOP = (tickets/15)* 100
-    const OP = document.getElementById('progress')
-
+    const progressBarOP = document.getElementById('progressOctoPlush')
     progressBarOP.style.width = `${progressOP}%`
+    const metaLabel = document.getElementById("octoPlushProgress");
+    metaLabel.textContent = `${tickets} / 15`;
 }
+
+const updateStatsW = () => {
+    const progressW = (tickets/25)* 100
+    const progressBarW = document.getElementById('progressWacom')
+    progressBarW.style.width = `${progressW}%`
+    const metaLabel = document.getElementById("wacomProgress");
+    metaLabel.textContent = `${tickets} / 25`;
+}
+
+
+const updateStatsB = () => {
+    const progressB = (tickets/50)* 100
+    const progressBarB = document.getElementById('progressBackpack')
+    progressBarB.style.width = `${progressB}%`
+    const metaLabel = document.getElementById("backpackProgress");
+    metaLabel.textContent = `${tickets} / 50`;
+}
+
+const updateStatsK = () => {
+    const progressK = (tickets/75)* 100
+    const progressBarK = document.getElementById('progressKeyboard')
+    progressBarK.style.width = `${progressK}%`
+    const metaLabel = document.getElementById("keyboardProgress");
+    metaLabel.textContent = `${tickets} / 75`;
+}
+
+
+const updateStatsFS = () => {
+    const progressFS = (tickets/120)* 100
+    const progressBarFS = document.getElementById('progressFrameworkSeconds')
+    progressBarFS.style.width = `${progressFS}%`
+    const metaLabel = document.getElementById("frameworkSecondsProgress");
+    metaLabel.textContent = `${tickets} / 120`;
+}
+
+const updateStats13 = () => {
+    const progress13 = (tickets/175)* 100
+    const progressBar13 = document.getElementById('progressFramework13Inch')
+    progressBar13.style.width = `${progress13}%`
+    const metaLabel = document.getElementById("framework13InchProgress");
+    metaLabel.textContent = `${tickets} / 175`;
+
+}
+
 
